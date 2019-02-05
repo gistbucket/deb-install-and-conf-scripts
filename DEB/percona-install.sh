af715d084df393f299c9f94ef0b25c026b905f12
JOduMonT <jodumont@users.noreply.github.com>
2019-02-05 10:36:06 +0100

    add rsa generation

--- DEB/percona-install.sh
+++ DEB/percona-install.sh
@@ -3,3 +3,9 @@ dpkg -i /tmp/percona.deb
 apt update
 lastPercona=$(apt search percona-server-server-|grep "percona-server-server-"|cut -d'/' -f1|tail -1|rev|cut -d- -f1|rev)
 apt install -y percona-server-server-$lastPercona percona-server-client-$lastPercona
+
+openssl genrsa -out /var/lib/mysql/private_key.pem 2048
+openssl rsa -in /var/lib/mysql/private_key.pem -pubout -out /var/lib/mysql/public_key.pem
+chmod 0400 /var/lib/mysql/private_key.pem
+chmod 0444 /var/lib/mysql/public_key.pem
+chown mysql:mysql /var/lib/mysql/*.pem
