f868ebcc3b0b78a5923d51a62ab5e62001c32392
JOduMonT <jodumont@users.noreply.github.com>
2019-03-19 11:05:43 +0100

    Create arpon-install.sh

---
+++ Debian/arpon-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt install -y arpon
+
+echo "
+DAEMON_OPTS=\"-q -f /var/log/arpon/arpon.log -g -D\"
+RUN=\"yes\"
+" > /etc/default/arpon
+
+systemctl enable arpon
+systemctl restart arpon
