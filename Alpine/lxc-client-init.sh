de8ac185bcdac4535df2264db3d1d0b4bfdfcb7c
JOduMonT <jodumont@users.noreply.github.com>
2019-03-29 15:56:33 +0100

    Update lxc-client-init.sh

--- Alpine/lxc-client-init.sh
+++ Alpine/lxc-client-init.sh
@@ -11,5 +11,5 @@ usermod -s /bin/bash root
 
 groupmod -g 1000 users
 useradd -Ng 1000 -mu 1000 -s /bin/bash rescue
-cp -r /root/.ssh /home/rescue/
+mv /root/.ssh /home/rescue/
 chown -R rescue:users /home/rescue/.ssh
