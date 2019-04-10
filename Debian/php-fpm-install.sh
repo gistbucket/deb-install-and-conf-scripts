ef2186a8eb5fda4976d537ef06526ded18a34f16
JOduMonT <jodumont@users.noreply.github.com>
2019-04-10 14:12:14 +0200

    Update php-fpm-install.sh

--- Debian/php-fpm-install.sh
+++ Debian/php-fpm-install.sh
@@ -1,3 +1,4 @@
 apt install -y apt-transport-https ca-certificates
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ stretch main" > /etc/apt/sources.list.d/php.list
+apt update
