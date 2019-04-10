b6a3baf1940b4f4a9c5d85558182b8fc368ea71c
JOduMonT <jodumont@users.noreply.github.com>
2019-04-10 19:26:28 +0200

    Update and rename docker-compose_version.sh to docker-compose.sh

---
+++ Debian/docker-compose.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+DockerComposeVersion="$(git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1)"
+
+curl -L "https://github.com/docker/compose/releases/download/${DockerComposeVersion}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+chmod a+x /usr/local/bin/docker-compose
