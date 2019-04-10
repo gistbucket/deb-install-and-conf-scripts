d6cc097e76654ff66f0202f8044a7500a91747aa
JOduMonT <jodumont@users.noreply.github.com>
2019-04-10 13:34:44 +0200

    Create php-fpm-install.sh

---
+++ Debian/php-fpm-install.sh
@@ -0,0 +1,3 @@
+apt install -y apt-transport-https ca-certificates
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ stretch main" > /etc/apt/sources.list.d/php.list
