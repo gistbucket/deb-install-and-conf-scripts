c51cdd2107915576744183ed98a9c8a2884f7ce6
JOduMonT <jodumont@users.noreply.github.com>
2019-04-10 21:19:38 +0200

    Update apparmor-install.sh

--- Debian/apparmor-install.sh
+++ Debian/apparmor-install.sh
@@ -3,14 +3,5 @@
 apt update
 apt install -y apparmor apparmor-profiles apparmor-utils
 
-if [ "$seconds" -eq 0 ];then
-  sed 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="apparmor=1 security=apparmor cgroup_enable=memory swapaccount=1 panic_on_oops=1 panic=5"/g' /etc/default/grub
-  update-grub
-
-elif[ "$seconds" -gt 0 ];then
-  sed 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="apparmor=1 security=apparmor cgroup_enable=memory swapaccount=1 panic_on_oops=1 panic=5"/g' /etc/default/grub
-  update-grub
-
-else
-   echo "Sorry you must edit manually '/etc/default/grub' and add 'apparmor=1 security=apparmor cgroup_enable=memory swapaccount=1 panic_on_oops=1 panic=5' at the end of GRUB_CMDLINE_LINUX_DEFAULT= line"
-fi
+sed 's|^\(GRUB_CMDLINE_LINUX_DEFAULT="quiet\)"$|\1 apparmor=1 security=apparmor cgroup_enable=memory swapaccount=1 panic_on_oops=1 panic=5"|' -i /etc/default/grub
+update-grub
