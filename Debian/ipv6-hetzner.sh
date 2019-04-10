f60386a46d6420ad6389cd052d3b37ba290a5eba
JOduMonT <jodumont@users.noreply.github.com>
2019-04-10 21:18:57 +0200

    Create ipv6-hetzner.sh

---
+++ Debian/ipv6-hetzner.sh
@@ -0,0 +1,14 @@
+curl -fsSLo /etc/resolv.conf https://raw.githubusercontent.com/jodumont/ConFig/master/etc/resolv.conf
+chattr +i /etc/resolv.conf
+
+echo -e "
+#iface ens3 inet static
+#  address /32
+#  gateway 172.31.1.1
+#  pointopoint 172.31.1.1
+
+#iface ens3 inet6 static
+#  address /64
+#  gateway fe80::1" >> /etc/network/interface
+
+nano /etc/network/interface
