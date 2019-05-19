ab3257a1790147aa2f5882f29689762557b374cb
JOduMonT <jodumont@users.noreply.github.com>
2019-05-19 21:48:51 +0200

    Update ddclient-install.sh

--- Debian/ddclient-install.sh
+++ Debian/ddclient-install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+#ref: https://github.com/ddclient/ddclient
+
 apt install -y libdata-validate-ip-perl libjson-any-perl libio-socket-ssl-perl
 
 cd /tmp
 git clone https://github.com/ddclient/ddclient.git
 cd /tmp/ddclient
 cp ddclient /usr/sbin/
-mkdir -p /etc/ddclient /var/cache/ddclient
-
 cp sample-etc_ddclient.conf /etc/ddclient/ddclient.conf
+
+# EDIT CONFIG -> /etc/ddclient/ddclient.conf
+
 cp sample-etc_rc.d_init.d_ddclient.ubuntu /etc/init.d/ddclient
 systemctl enable ddclient
+systemctl start ddclient
