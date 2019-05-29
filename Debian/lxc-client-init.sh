d6139ba55759beb334b4ad09cb9804dbca9cad66
JOduMonT <jodumont@users.noreply.github.com>
2019-05-29 21:09:36 +0200

    Update lxc-client-init.sh

--- Debian/lxc-client-init.sh
+++ Debian/lxc-client-init.sh
@@ -1,11 +1,26 @@
-sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen
-locale-gen
+apt update
+apt -o Dpkg::Options::="--force-confold" -y --force-yes install libtext-iconv-perl 
 
-[ "$(grep LC_ALL /etc/environment)" ] || echo 'LC_ALL="C"' >> /etc/environment
-source /etc/environment
-export LC_ALL
+                    
+[ "$(grep LC_ALL /etc/bash.bashrc)" ] || echo -e '
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8' >> /etc/bash.bashrc
 
-DEBIAN_FRONTEND=noninteractive
+[ "$(grep LC_ALL /etc/environment)" ] || echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+[ "$(grep LC_ALL /etc/locale.conf)" ] || echo "LANG=en_US.UTF-8" > /etc/locale.conf
+[ "$(grep LC_ALL /etc/locale.gen)" ] || echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+
+
+
+
+
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=C
+locale-gen en_US.UTF-8
+
 [ "$(grep force- /etc/dpkg/dpkg.cfg)" ] || echo -e "
 force-confold
 force-confdef
@@ -19,13 +34,9 @@ apt install -y \
     ca-certificates coreutils curl \
     debian-goodies debsums \
     gnupg \
-    haveged \
     lsb-release \
     unattended-upgrades \
     wget
-
-systemctl enable haveged
-systemctl start haveged
 
 cp /usr/share/unattended-upgrades/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
 sed -e 's|^//Unattended-Upgrade::AutoFixInterruptedDpkg.*;|Unattended-Upgrade::AutoFixInterruptedDpkg "true";|g' \
