9c82e823bd33556312172b10c5a8ed88ab9048f1
JOduMonT <jodumont@users.noreply.github.com>
2019-06-09 02:53:48 +0200

    Update omv-fix_weakref.sh

--- Debian/omv-fix_weakref.sh
+++ Debian/omv-fix_weakref.sh
@@ -1,5 +1,2 @@
-#!/bin/bash
-
 #ref: https://forum.openmediavault.org/index.php/Thread/19658-Upgrade-Debian-9-and-4-x/?pageNo=5
-
 wget -O /usr/lib/python3.5/weakref.py https://raw.githubusercontent.com/python/cpython/9cd7e17640a49635d1c1f8c2989578a8fc2c1de6/Lib/weakref.py
