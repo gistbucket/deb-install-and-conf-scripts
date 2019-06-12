985755b5feefc5c8f4f711e22968485f296224b3
JOduMonT <jodumont@users.noreply.github.com>
2019-06-12 20:06:02 +0200

    Update proxmox-pmg-install.sh

--- Debian/proxmox-pmg-install.sh
+++ Debian/proxmox-pmg-install.sh
@@ -1,7 +1,13 @@
+## BASED ON https://github.com/hetzneronline/installimage/blob/master/post-install/proxmox5
+## MUST HAVE AN IP FIX in /etc/network/interface
+## EDIT /etc/hosts ref: https://pve.proxmox.com/wiki/Install_Proxmox_VE_on_Debian_Stretch#Install_a_standard_Debian_Stretch_.28amd64.29
+
 export LANG="en_US.UTF-8"
 export LC_ALL="C"
 
 echo -e "Acquire::ForceIPv4 \"true\";\\n" > /etc/apt/apt.conf.d/99force-ipv4
+
+sed -e "s// pvelocalhost/" -i /etc/hosts                                    
 
 [[ -z $(grep contrib /etc/apt/sources.list) ]] && \
 sed -e "s/ main/ main contrib/" \
@@ -13,5 +19,17 @@ wget http://download.proxmox.com/debian/proxmox-ve-release-5.x.gpg -O /etc/apt/t
 
 apt update
 apt install -y byobu debian-archive-keyring debian-goodies etckeeper fail2ban ipset pigz
+
+cat <<EOF | debconf-set-selections
+postfix postfix/mailname           string $(cat /etc/hostname)
+postfix postfix/destinations       string $(cat /etc/hostname), proxmox, localhost.localdomain, localhost
+postfix postfix/chattr             boolean false
+postfix postfix/mailbox_limit      string 0
+postfix postfix/main_mailer_type   select Local only
+postfix postfix/mynetworks         string 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128
+postfix postfix/protocols          select all
+postfix postfix/recipient_delim    string +
+postfix postfix/rfc1035_violation  boolean false
+EOF
 
 apt install -y proxmox-mailgateway
