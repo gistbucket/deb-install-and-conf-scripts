1044524bdec5f47877a566097ade35aa35cf7b3c
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2019-08-30 06:53:08 +0700

    Create base.sh

---
+++ base.sh
@@ -0,0 +1,13 @@
+apt update
+apt install -y libtext-iconv-perl
+apt dist-upgrade -y
+apt install -y \
+    apt-listchanges apt-transport-https \
+    ca-certificates coreutils curl \
+    debian-goodies debsums \
+    gnupg \
+    lsb-release \
+    unattended-upgrades \
+    wget
+
+timedatectl set-ntp 1
