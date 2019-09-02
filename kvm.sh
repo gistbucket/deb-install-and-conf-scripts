225a6c0a9c2e11ca758ac05ceddcc953b3792a5c
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2019-09-02 15:54:23 +0700

    Update kvm.sh

--- kvm.sh
+++ kvm.sh
@@ -1,2 +1,4 @@
 apt remove --purge netcat
 apt -y install qemu-kvm bridge-utils libvirt-daemon libvirt-daemon-system netcat-openbsd
+
+echo "RECOMMENDED: adding a user in libvirt group with usermod -aG libvirt $USER"
