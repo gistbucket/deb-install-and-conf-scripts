ba629beff8e07d15aa86009f1d96421a24f7f2ef
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2019-09-03 14:07:09 +0700

    Update kvm.sh

--- kvm.sh
+++ kvm.sh
@@ -1,4 +1,11 @@
 apt remove --purge netcat
 apt -y install qemu-kvm bridge-utils libvirt-daemon libvirt-daemon-system netcat-openbsd
 
+cat >> /etc/sysctl.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 0
+net.bridge.bridge-nf-call-iptables = 0
+net.bridge.bridge-nf-call-arptables = 0
+EOF
+sysctl -p /etc/sysctl.conf
+ 
 echo "RECOMMENDED: adding a user in libvirt group with usermod -aG libvirt $USER"
