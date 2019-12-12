a4a0378804aefcf291efb0550a3dff19edbe5011
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2019-12-12 13:10:20 +0700

    Create desktop-fonts.sh

---
+++ desktop-fonts.sh
@@ -0,0 +1,5 @@
+apt install -y \
+fonts-{dejavu,dejavu-{core,extra},freefont-{otf,ttf},\
+roboto,roboto-{hinted,slab,unhinted},\
+symbola} \
+ttf-{ancient-fonts-symbola,anonymous-pro,dejavu-{core,extra},mscorefonts-installer}
