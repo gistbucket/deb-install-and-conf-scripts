5651e47eb449bc899cee52fc65464803f331ae8e
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-01-06 08:16:13 +0700

    Update omv-1strun.sh

--- omv-1strun.sh
+++ omv-1strun.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -eux
 
-rm /var/cache/apt/archives/lock
+## fix the weakref.py error
+wget -O /usr/lib/python3.5/weakref.py https://raw.githubusercontent.com/python/cpython/9cd7e17640a49635d1c1f8c2989578a8fc2c1de6/Lib/weakref.py
 
+## install extras
+wget -O - http://omv-extras.org/install | bash
+
+## remove firmware
+rm /var/cache/apt/archives/lock
 apt remove -y --purge amd64-microcode \
   firmware-amd-graphics firmware-atheros \
   firmware-bnx2 firmware-bnx2x firmware-brcm80211 \
@@ -24,21 +30,13 @@ apt install -y apparmor apparmor-profiles apparmor-utils \
   git \
   libvirt-daemon libvirt-daemon-system \
   netcat-openbsd \
+  openmediavault-backup openmediavault-clamav openmediavault-diskstats openmediavault-fail2ban openmediavault-locate openmediavault-luksencryption openmediavault-resetperms openmediavault-usbbackup \
   qemu-kvm qemu-utils qemu-system-arm qemu-system-x86
 
 apt upgrade -y
 
-## fix the weakref.py error
-wget -O /usr/lib/python3.5/weakref.py https://raw.githubusercontent.com/python/cpython/9cd7e17640a49635d1c1f8c2989578a8fc2c1de6/Lib/weakref.py
-
-## install extras
-wget -O - http://omv-extras.org/install | bash
-
-apt update
-apt upgrade -y
-
 [[ -z cat /proc/cpuinfo|grep endor|grep ntel ]] && \
-  sed 's|^\(GRUB_CMDLINE_LINUX_DEFAULT="quiet\)"$|\1 intel_iommu=on apparmor=1 security=apparmor cgroup_enable=memory swapaccount=1 panic_on_oops=1 panic=5"|' -i /etc/default/grub
+  sed 's|^\(GRUB_CMDLINE_LINUX_DEFAULT="\)"$|\1 intel_iommu=on apparmor=1 security=apparmor cgroup_enable=memory swapaccount=1 panic_on_oops=1 panic=5"|' -i /etc/default/grub
 
 update-grub
 
