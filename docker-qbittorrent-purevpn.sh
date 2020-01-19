d3668b75b98396a10d739901be8e0357b3724be8
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-01-20 00:34:41 +0700

    Update docker-qbittorrent-purevpn.sh

--- docker-qbittorrent-purevpn.sh
+++ docker-qbittorrent-purevpn.sh
@@ -5,25 +5,24 @@
 ### run as usual your linuxserver/qbittorrent container
 
 CONTAINER=
-HOME=/config
 PUREVPN-USERNAME=""
 PUREVPN-PASSWORD=""
 COUNTRY=""
 
-docker exec -ti {CONTAINER:-qbittorrent} apt update
-docker exec -ti {CONTAINER:-qbittorrent} apt install -y openvpn
-docker exec -ti {CONTAINER:-qbittorrent} wget https://s3-us-west-1.amazonaws.com/heartbleed/windows/New+OVPN+Files.zip
-docker exec -ti {CONTAINER:-qbittorrent} unzip -ojd $HOME/.purevpn New+OVPN+Files.zip
-docker exec -ti {CONTAINER:-qbittorrent} rm New+OVPN+Files.zip
-docker exec -ti {CONTAINER:-qbittorrent} rm -Rf $HOME/.purevpn/*udp*
-docker exec -ti {CONTAINER:-qbittorrent} sed -i "s/auth-user-pass.*/auth-user-pass $HOME\/.purevpn-login.conf/" *.ovpn
-docker exec -ti {CONTAINER:-qbittorrent} sed -i '/^route-.*/d' *.ovpn
-docker exec -ti {CONTAINER:-qbittorrent} sed -i 's/^route.*/route-nopull/' *.ovpn
+docker exec -t ${CONTAINER:-qbittorrent} apt update
+docker exec -t ${CONTAINER:-qbittorrent} apt install -y openvpn wget
+docker exec -t ${CONTAINER:-qbittorrent} wget https://s3-us-west-1.amazonaws.com/heartbleed/windows/New+OVPN+Files.zip
+docker exec -t ${CONTAINER:-qbittorrent} unzip -ojd /config/.purevpn New+OVPN+Files.zip
+docker exec -t ${CONTAINER:-qbittorrent} rm New+OVPN+Files.zip
+docker exec -t ${CONTAINER:-qbittorrent} rm -Rf /config/.purevpn/*udp*
+docker exec -t ${CONTAINER:-qbittorrent} sed -i "s/auth-user-pass.*/auth-user-pass \/config\/.purevpn-login.conf/" /config/.purevpn/*.ovpn
+docker exec -t ${CONTAINER:-qbittorrent} sed -i '/^route-.*/d' /config/.purevpn/*.ovpn
+docker exec -t ${CONTAINER:-qbittorrent} sed -i 's/^route.*/route-nopull/' /config/.purevpn/*.ovpn
 
 ## PUT YOUR USERNAME PASSWORD IN /config/.purevpn-login.conf
-docker exec -ti {CONTAINER:-qbittorrent} echo $PUREVPN-USERNAME > $HOME/.purevpn-login.conf
-docker exec -ti {CONTAINER:-qbittorrent} echo $PUREVPN-PASSWORD >> $HOME/.purevpn-login.conf
-docker exec -ti {CONTAINER:-qbittorrent} chmod 0400 $HOME/.purevpn-login.conf
+docker exec -t {CONTAINER:-qbittorrent} echo $PUREVPN-USERNAME > $HOME/.purevpn-login.conf
+docker exec -t {CONTAINER:-qbittorrent} echo $PUREVPN-PASSWORD >> $HOME/.purevpn-login.conf
+docker exec -t {CONTAINER:-qbittorrent} chmod 0400 $HOME/.purevpn-login.conf
 
 ## ESTABLISH CONNECTION FROM THE HOST
 docker exec -ti ${CONTAINER:-qbittorrent} openvpn /config/.purevpn/${COUNTRY:-nl}2-ovpn-tcp-tcp.ovpn &
