c0f6f8825673982dad5c359aa4558e8f448594ca
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-01-19 11:49:35 +0700

    Update omv-1strun.sh

--- omv-1strun.sh
+++ omv-1strun.sh
@@ -30,7 +30,7 @@ apt install -y apparmor apparmor-profiles apparmor-utils \
   git \
   libvirt-daemon libvirt-daemon-system \
   netcat-openbsd \
-  openmediavault-backup openmediavault-clamav openmediavault-diskstats openmediavault-fail2ban openmediavault-locate openmediavault-luksencryption openmediavault-resetperms openmediavault-usbbackup openmediavault-ups \
+  openmediavault-backup openmediavault-clamav openmediavault-diskstats openmediavault-fail2ban openmediavault-locate openmediavault-luksencryption openmediavault-nut openmediavault-resetperms openmediavault-usbbackup \
   powertop \
   qemu-kvm qemu-utils qemu-system-arm qemu-system-x86
 
