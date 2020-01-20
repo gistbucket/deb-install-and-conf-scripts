f62f413a3265c6facc87a09e7962d7dbf03c7124
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-01-20 22:05:12 +0700

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,9 +1,24 @@
-sudo apt-get remove -y \
+[ "$(grep LC_ALL /etc/bash.bashrc)" ] || echo -e '
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export LANGUAGE="en_US:en"
+export LANG="en_US.UTF-8"
+export LC_ALL="C"
+' >> /etc/bash.bashrc
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export LANGUAGE=en_US:en
+export LANG=en_US.UTF-8
+export LC_ALL=C
+locale-gen en_US.UTF-8
+
+apt update
+
+apt-get remove -y \
   containerd \
   docker docker-engine docker.io \
   runc
 
-sudo apt-get install -y \
+apt-get install -y \
   apt-transport-https \
   ca-certificates curl \
   gnupg2 \
@@ -11,19 +26,19 @@ sudo apt-get install -y \
   wget
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | \
-  sudo apt-key add -
+  apt-key add -
 
 # if Ubuntu == $(lsb_release -i|cut -d\: -f2)
 # else OS= :-debian
 #echo "deb [arch=amd64] https://download.docker.com/linux/${OS} $(lsb_release -cs) stable" | \
 echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list
+  tee /etc/apt/sources.list.d/docker.list
 
-sudo apt update
+apt update
 
-sudo apt-get install -y \
+apt-get install -y \
   containerd.io \
   docker-ce docker-ce-cli
 
-sudo apt remove --purge -y \
+apt remove --purge -y \
   aufs*
