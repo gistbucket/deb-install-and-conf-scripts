97615c08922cc159a277f3a45b7fb191d31f0826
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-01-21 07:03:58 +0700

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,5 +1,5 @@
 DockerComposeVersion="$(git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1)"
 
-sudo curl -L "https://github.com/docker/compose/releases/download/${DockerComposeVersion}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
-sudo chown :docker /usr/local/bin/docker-compose
-sudo chmod 750 /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/${DockerComposeVersion}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+chown :docker /usr/local/bin/docker-compose
+chmod 750 /usr/local/bin/docker-compose
