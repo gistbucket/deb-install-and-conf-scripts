3d0d5238954d720672831deddf245c62110aa41d
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-01-21 14:33:15 +0700

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,12 +1,14 @@
 apt update
 apt install -y libtext-iconv-perl
 apt dist-upgrade -y
+
 apt install -y \
     apt-listchanges apt-transport-https \
     ca-certificates coreutils curl \
     debian-goodies debsums dirmngr \
     gnupg \
     lsb-release \
+    net-tools \
     unattended-upgrades \
     wget
 
