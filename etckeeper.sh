31afe9fbec181abe640bba5cf957640dbc880512
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-02-02 21:18:08 +0700

    Update etckeeper.sh

--- etckeeper.sh
+++ etckeeper.sh
@@ -8,9 +8,13 @@ apt install -y etckeeper
 sed -i "s/^VCS=.*/VCS=\"git\"/" /etc/etckeeper/etckeeper.conf
 sed -i "s/^PUSH_REMOTE=.*/PUSH_REMOTE=\"origin\"/" /etc/etckeeper/etckeeper.conf
 
+mkdir -p /etc/.github/ISSUE_TEMPLATE/
+wget -O /etc/.github/ISSUE_TEMPLATE/bug_report.md https://raw.githubusercontent.com/JOduMonT/.dotfile/master/.github/ISSUE_TEMPLATE/bug_report.md
+wget -O /etc/.github/ISSUE_TEMPLATE/feature_request.md https://raw.githubusercontent.com/JOduMonT/.dotfile/master/.github/ISSUE_TEMPLATE/feature_request.md
+
 cd /etc
 etckeeper init
+etckeeper add -A
 etckeeper commit -m "init"
 git remote add origin ${PRIVATEREPO}
 git push -u origin master
-exit
