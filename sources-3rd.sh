0b72bd9c0d00fb84f01df7e6ebf1a611c85147d9
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-02-20 17:03:20 +0700

    Update sources-3rd.sh

--- sources-3rd.sh
+++ sources-3rd.sh
@@ -1,48 +1,54 @@
+#!/bin/bash -eux
+
+OS=$(lsb_release -s -i)
+VERSION=$(lsb_release -s -c)
+
+## ADAPTA THEME
 apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys EAC0D406E5D79A82ADEEDFDFB76E53652D87398A
+echo "deb http://ppa.launchpad.net/tista/adapta/${OS,,} $VERSION main" > /etc/apt/sources.list.d/adapta.list
+
+## DEB MULTIMEDIA
 wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb && dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+echo "deb [arch=amd64] https://www.deb-multimedia.org $VERSION main non-free" > /etc/apt/sources.list.d/deb-multimedia.list
+
+## DOCKER
+curl -fsSL https://download.docker.com/linux/${OS,,}/gpg | apt-key add -
+echo "deb [arch=amd64] https://download.docker.com/linux/${OS,,} $VERSION stable" > /etc/apt/sources.list.d/docker.list
+
+## GOOGLE
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C80E383C3DE9F082E01391A0366C67DE91CA5D5F
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xF1656F24C74CD1D8
-wget -q -O - http://download.opensuse.org/repositories/home:/ivaradi/Debian_9.0/Release.key | apt-key add -
-wget https://nginx.org/keys/nginx_signing.key && apt-key add nginx_signing.key
-curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
-wget -q -O- https://packages.sury.org/php/apt.gpg | apt-key add -
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 1F3045A5DF7587C3
-wget -O - https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc | apt-key add -
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg && mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-
-###Adapta GTK Theme
-echo "deb http://ppa.launchpad.net/tista/adapta/ubuntu bionic main" > /etc/apt/sources.list.d/adapta.list
-
-###Debian Multimedia
-echo "deb [arch=amd64] https://www.deb-multimedia.org buster main non-free" > /etc/apt/sources.list.d/deb-multimedia.list
-
-###Docker CE
-echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" > /etc/apt/sources.list.d/docker-ce.list
-
-###Google Chrome Browser
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 
-###MariaDB
-echo "deb [arch=amd64] http://mirror.23media.de/mariadb/repo/10.2/debian stretch main" > /etc/apt/sources.list.d/mariadb.list
+## MARIADB
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C80E383C3DE9F082E01391A0366C67DE91CA5D5F
+echo "deb [arch=amd64] http://mirror.23media.de/mariadb/repo/10.2/${OS,,} $VERSION main" > /etc/apt/sources.list.d/mariadb.list
 
-###nginx
-echo "deb [arch=amd64] http://nginx.org/packages/debian/ buster nginx" > /etc/apt/sources.list.d/nginx.list
+### NGINX
+wget https://nginx.org/keys/nginx_signing.key && apt-key add nginx_signing.key
+echo "deb [arch=amd64] http://nginx.org/packages/${OS,,} $VERSION nginx" > /etc/apt/sources.list.d/nginx.list
 
-###NodeJs
-echo "deb https://deb.nodesource.com/node_12.x buster main" > /etc/apt/sources.list.d/nodejs.list
+## NODEJS
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+echo "deb https://deb.nodesource.com/node_12.x $VERSION main" > /etc/apt/sources.list.d/nodejs.list
 
-###PHP
-echo "deb https://packages.sury.org/php/ buster main" > /etc/apt/sources.list.d/php.list
+## PHP
+wget -q -O- https://packages.sury.org/php/apt.gpg | apt-key add -
+echo "deb https://packages.sury.org/php/ $VERSION main" > /etc/apt/sources.list.d/php.list
 
-###Skype
+## POSTGRESQL
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $VERSION-pgdg main" > /etc/apt/sources.list.d/postgres.list
+
+## SKYPE
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 1F3045A5DF7587C3
 echo "deb [arch=amd64] https://repo.skype.com/deb stable main" > /etc/apt/sources.list.d/skype.list
 
-###Teamviewer
+## TEAMVIEWER
+wget -O - https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc | apt-key add -
 echo "deb http://linux.teamviewer.com/deb stable main" > /etc/apt/sources.list.d/teamviewer.list
 
-###Visual Studio Code
+## VSCODE
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg && mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 
 apt update
