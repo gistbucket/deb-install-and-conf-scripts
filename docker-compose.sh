a475e76f25140467de4fee8b3701e5e7f096e1a6
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-02-27 20:30:37 +0700

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,9 +1,5 @@
-DockerComposeVersion="$(git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1)"
+DockerComposeVersion="$(curl -fsSL https://github.com/docker/compose/releases/latest|grep Release|grep title|rev|cut -d' ' -f5|rev)"
 
 curl -L "https://github.com/docker/compose/releases/download/${DockerComposeVersion}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chown :docker /usr/local/bin/docker-compose
 chmod 750 /usr/local/bin/docker-compose
-
-#apt install -y python3-pip
-#pip3 install --upgrade pip
-#pip3 install docker-compose
