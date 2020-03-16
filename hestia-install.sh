33c917ac0fd90ed89d608e4e0d30100680b7dec8
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-03-16 09:17:36 +0700

    Update hestia-install.sh

--- hestia-install.sh
+++ hestia-install.sh
@@ -1,22 +1,48 @@
+#!/bin/bash -eu
+
+echo -e "[user]
+        name = $(hostname -s)
+        email = hostmaster@$(hostname -d)" > /root/.gitconfig
+apt install -y etckeeper
+
 PASSWORD=$(< /dev/urandom tr -dc @.,/=_A-Z-a-z-0-9 | head -c24)
 REDISPWD=$(< /dev/urandom tr -dc @.,/=_A-Z-a-z-0-9 | head -c24)
 
-[ "$(grep LC_ALL /etc/bash.bashrc)" ] && \
-cat <<EOF >> /etc/bash.bashrc
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-export LANGUAGE="en_US:en"
-export LANG="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
-EOF
-
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export LANGUAGE=en_US:en
 export LANG=en_US.UTF-8
 export LC_ALL=C
 locale-gen en_US.UTF-8
 
-apt -y remove --purge ufw postfix
-#rm /etc/apt/apt.conf.d/01-vendor-ubuntu
+REMOVE="apport* autofs avahi* beep btrfs-* cryptsetup cryptsetup-bin dmsetup eatmydata lxcfs lxd lxd-client lvm2 mdadm ntfs-3g open-iscsi pastebinit popularity-contest postfix prelink rsh* rsync snapd talk* telnet* tftp* ubuntu-report ufw xfsprogs whoopsie xinetd yp-tools ypbind"
+## REMOVE PACKAGES
+for package in $REMOVE; do
+  apt purge -y $package
+done
+
+## DISABLE NET
+NETPROTOCOLS="dccp sctp rds tipc"
+for disable in $NETPROTOCOLS; do
+  if ! grep -q $disable /etc/modprobe.d/disablenet.conf 2> /dev/null; then
+    echo "install $disable /bin/true" >> /etc/modprobe.d/disablenet.conf
+  fi
+done
+
+## DISABLE MISC FILE SYSTEM
+FILESYSTEMS="cramfs freevxfs jffs2 hfs hfsplus squashfs udf vfat"
+for disable in $FILESYSTEMS; do
+  if ! grep -q $disable /etc/modprobe.d/disablefs.conf 2> /dev/null; then
+    echo "install $disable /bin/true" >> /etc/modprobe.d/disablefs.conf
+  fi
+done
+
+## DISABLE MODULES
+MODULES="bluetooth bnep btusb cpia2 firewire-core floppy n_hdlc net-pf-31 pcspkr soundcore thunderbolt usb-midi usb-storage uvcvideo v4l2_common"
+for disable in $MODULES; do
+  if ! grep -q $disable /etc/modprobe.d/disablemod.conf 2> /dev/null; then
+    echo "install $disable /bin/true" >> /etc/modprobe.d/disablemod.conf
+  fi
+done
+
 wget https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install.sh
 bash hst-install.sh \
 -a no \
@@ -31,11 +57,7 @@ bash hst-install.sh \
 -e hostmaster@$(hostname -d) \
 -p $PASSWORD
 
-echo -e "[user]
-        name = $(hostname -s)
-        email = hostmaster@$(hostname -d)" > /root/.gitconfig
-
-apt install -y curl etckeeper php7.2-gmp php-imagick php-smbclient php-redis redis-server wget
+apt install -y curl php7.2-gmp php-imagick php-smbclient php-redis redis-server wget
 
 [[ -z $REDISPWD ]] && \
 #REDISPWD=$(openssl rand -base64 24)
