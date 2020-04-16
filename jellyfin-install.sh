3b537318512e0d2ffa605ea20acc4974334343e3
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-04-16 16:14:33 +0700

    Update and rename jellyfin-install.sh to install-jellyfin

--- jellyfin-install.sh
+++
@@ -1,7 +0,0 @@
-apt install -y apt-transport-https
-
-wget -O - https://repo.jellyfin.org/debian/jellyfin_team.gpg.key | apt-key add -
-echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/debian $( lsb_release -c -s ) main" | tee /etc/apt/sources.list.d/jellyfin.list
-
-apt update
-apt install -y jellyfin
