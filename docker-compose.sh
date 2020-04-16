9c7b7a5f36e20082164f4175e2f253656a772f88
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-04-16 15:26:14 +0700

    Update and rename docker-compose.sh to snipperts/install-dockercompose

--- docker-compose.sh
+++
@@ -1,5 +0,0 @@
-DockerComposeVersion="$(curl -fsSL https://github.com/docker/compose/releases/latest|grep Release|grep title|rev|cut -d' ' -f5|rev)"
-
-curl -L "https://github.com/docker/compose/releases/download/${DockerComposeVersion}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chown :docker /usr/local/bin/docker-compose
-chmod 750 /usr/local/bin/docker-compose
