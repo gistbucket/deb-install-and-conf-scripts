e7cbfec71803010c0c2276acf7cf4e3508cef1e6
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-04-21 14:42:55 +0700

    Create rsync.sh

---
+++ rsync.sh
@@ -0,0 +1,8 @@
+#    -h: human readable numbers
+#    -v: verbose
+#    -r: recurse into directories
+#    -P: --partial (keep partially transferred files) +
+#        --progress (show progress during transfer)
+#    -t: preserve modification times
+
+rsync -hPrtv $SOURCE $TARGET
