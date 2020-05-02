ce9cb5232f1f554626bc9c90c56901b58629c34d
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-05-02 09:29:41 +0700

    Update and rename 1strun-lxc.sh to 1strun-lxc-ct.sh

---
+++ 1strun-lxc-ct.sh
@@ -0,0 +1,4 @@
+for SCRIPT in dist-upgrade install-{autoupdate,base} config-{autoupdate,dpkg,locale-en_us,timedatectl} clean-{apt,history} ; do
+  wget -qO /tmp/$SCRIPT https://raw.githubusercontent.com/JOduMonT/DEB/master/snippets/$SCRIPT;
+  bash /tmp/$SCRIPT;
+done
--- 1strun-lxc.sh
+++
@@ -1,4 +0,0 @@
-for SCRIPT in dist-upgrade install-autoupdate config-{autoupdate,dpkg,locale-en_us,lxc,timedatectl} clean-{apt,history} ; do
-  wget -qO /tmp/$SCRIPT https://raw.githubusercontent.com/JOduMonT/DEB/master/snippets/$SCRIPT;
-  bash /tmp/$SCRIPT;
-done
