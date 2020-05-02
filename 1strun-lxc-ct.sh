a07ba3fcd868f14344d21ffb2ad9fa9834b288d5
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-05-02 10:07:04 +0700

    Update 1strun-lxc-ct.sh

--- 1strun-lxc-ct.sh
+++ 1strun-lxc-ct.sh
@@ -1,3 +1,4 @@
+/usr/bin/env DEBIAN_FRONTEND=noninteractive
 for SCRIPT in dist-upgrade install-{autoupdate,base} config-{autoupdate,dpkg,locale-en_us,timedatectl} clean-{apt,history} ; do
   wget -qO /tmp/$SCRIPT https://raw.githubusercontent.com/JOduMonT/DEB/master/snippets/$SCRIPT;
   bash /tmp/$SCRIPT;
