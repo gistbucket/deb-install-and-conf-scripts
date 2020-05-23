bd87014eb0223990380f2b192b945f29b43de20e
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-05-23 11:47:17 +0700

    Update install-nodejs.sh

--- snippets/install-nodejs.sh
+++ snippets/install-nodejs.sh
@@ -1,5 +1,6 @@
+#ref: https://github.com/nodesource/distributions/issues/938
 nodeVS=14
 
 apt install -y curl software-properties-common
-curl -sL https://deb.nodesource.com/setup_$nodeVS.x | bash -
+curl -sL https://node.melroy.org/deb/setup_12.x | bash -
 apt install -y nodejs
