5dd7e71d9f09a246feaae656540d57193132d68a
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-06-28 08:17:07 +0700

    Update 1strun-lxc-ct.sh

--- 1strun-lxc-ct.sh
+++ 1strun-lxc-ct.sh
@@ -1,4 +1,4 @@
-for SCRIPT in dist-upgrade install-{autoupdate,base} config-{autoupdate,dpkg,locale-en_us,timedatectl} clean-{apt,history} ; do
+for SCRIPT in config-locale-en_us dist-upgrade install-{autoupdate,base} config-{autoupdate,dpkg,timedatectl} clean-{apt,history} ; do
   wget -qO /tmp/$SCRIPT https://raw.githubusercontent.com/JOduMonT/DEB/master/snippets/$SCRIPT;
   bash /tmp/$SCRIPT;
 done
