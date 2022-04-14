b5c2acdcf54828c485b9a3b9e14a65c218958990
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2022-04-14 11:04:42 +0700

    automatically launch kodi as kodi user with nodm
    this script will install kodi and nodm so at the next boot kodi will launch automatically as kodi user

---
+++ install-kodi+nodm.sh
@@ -0,0 +1,16 @@
+# this script will install kodi and nodm so at the next boot kodi will launch automatically as kodi user
+# usage case: on top proxmox, openmediavault, raspbian, or any minimal debian installation
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt install -yq kodi nodm xinit
+
+useradd -mU kodi
+
+echo "exec /usr/bin/kodi-standalone" > /home/kodi/.xinitrc
+chown kodi:kodi /home/kodi/.xinitrc
+chmod +rx /home/kodi/.xinitrc
+
+sed -e "s|^NODM_USER=.*|NODM_USER=kodi|" \
+    -e "s|^NODM_XSESSION=.*|NODM_XSESSION=/home/kodi/.xinitrc|" \
+    -i /etc/default/nodm
