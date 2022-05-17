41ba6c4fcaa241c39b415d9bfa29361f4a267b63
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2022-05-17 10:14:33 +0700

    Create clean-firmware.sh

---
+++ snippets/clean-firmware.sh
@@ -0,0 +1,5 @@
+
+# for an obscure reason, Debian Testing (11) is installing all these differents firmware
+# for an even more obscure rease; this is anoying me.
+
+apt purge amd64-microcode arm-trusted-firmware-tools atmel-firmware dahdi-firmware-nonfree dfu-util dns323-firmware-tools firmware-ath9k-htc firmware-atheros firmware-bnx2 firmware-bnx2x firmware-brcm80211 firmware-cavium firmware-intelwimax firmware-ipw2x00 firmware-ivtv firmware-libertas firmware-microbit-micropython firmware-microbit-micropython-doc firmware-myricom firmware-netronome firmware-netxen firmware-qcom-media firmware-qcom-soc firmware-qlogic firmware-realtek firmware-samsung firmware-siano firmware-sof-signed firmware-ti-connectivity firmware-tomu firmware-zd1211 hdmi2usb-fx2-firmware ipxe-qemu midisport-firmware sigrok-firmware-fx2lafw ubertooth-firmware
